b78d51c0aee2b34941cba59d37a7efd6cead207e
Ingeborg Slegers <ingeborgfreda@gmail.com>
2019-06-18 14:55:00 -0400

    ideas on deconstruction

---
+++ deconstruction.js
@@ -0,0 +1,3 @@
+// * Deconstruction is a great tool to access take apart arrays and objects without altering the original variable.
+// * Objects use {} and elements are targeted by property name.
+// * Arrays use [] and elements are targeted by position/index.
